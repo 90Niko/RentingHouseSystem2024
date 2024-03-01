ef706ac5857bbcc3c46fdff701edb2aa8ee357b3
90Niko <110725784+90Niko@users.noreply.github.com>
2024-03-01 11:57:39 +0100

    Add first migration

--- ClassLibrary2/Data/ApplicationDbContext.cs
+++ ClassLibrary2/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace RentingHouseSystem.Data
+namespace RentingHouseSystem
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- RentingHouseSystem/Program.cs
+++ RentingHouseSystem/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using RentingHouseSystem.Data;
+using RentingHouseSystem;
 
 namespace RentingHouseSystem
 {
