ba774eb6270e7fe2419a95ba642314f4a67c131c
90Niko <110725784+90Niko@users.noreply.github.com>
2024-03-04 19:06:26 +0100

    Add and SaveChanges implemeted in Repository

--- ClassLibrary2/Data/Comman/Repository.cs
+++ ClassLibrary2/Data/Comman/Repository.cs
@@ -31,5 +31,15 @@ namespace RentingHouseSystem.Infrastructure.Data.Comman
         {
             return DbSet<T>();
         }
+
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await DbSet<T>().AddAsync(entity);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
     }
 }
