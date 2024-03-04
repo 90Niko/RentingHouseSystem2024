3d61e5293d8cbdc3d135b29f4688d055dadfe8fa
90Niko <110725784+90Niko@users.noreply.github.com>
2024-03-04 19:57:55 +0100

    Become Method

---
+++ RentingHouseSystem/Attributes/NotAnAgentAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using RentingHouseSystem.Core.Contracts.Agent;
+using RentingHouseSystem.Extensions;
+using ActionFilterAttribute = Microsoft.AspNetCore.Mvc.Filters.ActionFilterAttribute;
+using System.Security.Claims;
+
+
+
+namespace RentingHouseSystem.Attributes
+{
+    public class NotAnAgentAttribute : ActionFilterAttribute
+    {
+
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IAgentService? agentService = context.HttpContext.RequestServices.GetService<IAgentService>();
+
+            if (agentService == null)
+            {
+                context.Result = new BadRequestObjectResult("Agent service is not available");
+            }
+
+            if (agentService != null && agentService.ExistByIdAsync(context.HttpContext.User.Id()).Result)
+            {
+                context.Result = new BadRequestObjectResult("You are already an agent");
+            }
+        }
+    }
+}
--- RentingHouseSystem/Controllers/AgentController.cs
+++ RentingHouseSystem/Controllers/AgentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RentingHouseSystem.Attributes;
 using RentingHouseSystem.Core.Contracts;
 using RentingHouseSystem.Core.Contracts.Agent;
 using RentingHouseSystem.Core.Models.Agent;
@@ -21,26 +22,36 @@ namespace RentingHouseSystem.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Become()
+        [NotAnAgent]
+        public IActionResult Become()
         {
-            if (await agentService.ExistByIdAsync(User.Id()))
-            {
-                return BadRequest("You are already an agent");
-            }
-
             var model = new BecomeAgentFormModel();
-           
+
             return View(model);
         }
+
         [HttpPost]
+        [NotAnAgent]
         public async Task<IActionResult> Become(BecomeAgentFormModel model)
         {
-            if (!ModelState.IsValid)
+            if (ModelState.IsValid == false)
             {
                 return View(model);
             }
 
+            if (await agentService.UserHasRentsAsync(User.Id()))
+            {
+                ModelState.AddModelError(nameof(model.PhoneNumber), "You have rents and cannot become an agent");
+            }
+
+            if (await agentService.UserWithPhoneNumberAsync(User.Id()))
+            {
+                ModelState.AddModelError("Error", "You are already an agent");
+            }
+
             return RedirectToAction(nameof(HomeController.Index), "Home");
+
+            await agentService.CreateAsync(User.Id(), model.PhoneNumber);
         }
 
 
